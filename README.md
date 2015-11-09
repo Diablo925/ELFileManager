3eb4b840414ac2f3f6373a3191071ee51c36d6e9
Carsten Andersen <carsten@alio.dk>
2015-11-09 19:49:49 +0100

    version 1.0.3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ELFileManager
 
-Version: 1.0.2<br />
+Version: 1.0.3<br />
 [Forums](http://forums.sentora.org/showthread.php?tid=2076) 
 for comments, discussion, more information and community support.
 
@@ -17,3 +17,4 @@ And help from TGates [@github]
 1.0.0 testet
 1.0.1 never release
 1.0.2 release to github
+1.0.3 fix unpack zip files where .htaccess file are in 
--- app/php/connector.minimal.php
+++ app/php/connector.minimal.php
@@ -1,11 +1,39 @@
 <?php
+
 error_reporting(0); // Set E_ALL for debuging
 
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderConnector.class.php';
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinder.class.php';
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeDriver.class.php';
 include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeLocalFileSystem.class.php';
+// Required for MySQL storage connector
+// include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeMySQL.class.php';
+// Required for FTP connector support
+// include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeFTP.class.php';
 
+/**
+ * # Dropbox volume driver need "dropbox-php's Dropbox" and "PHP OAuth extension" or "PEAR's HTTP_OAUTH package"
+ * * dropbox-php: http://www.dropbox-php.com/
+ * * PHP OAuth extension: http://pecl.php.net/package/oauth
+ * * PEAR's HTTP_OAUTH package: http://pear.php.net/package/http_oauth
+ *  * HTTP_OAUTH package require HTTP_Request2 and Net_URL2
+ */
+// Required for Dropbox.com connector support
+// include_once dirname(__FILE__).DIRECTORY_SEPARATOR.'elFinderVolumeDropbox.class.php';
+
+// Dropbox driver need next two settings. You can get at https://www.dropbox.com/developers
+// define('ELFINDER_DROPBOX_CONSUMERKEY',    '');
+// define('ELFINDER_DROPBOX_CONSUMERSECRET', '');
+// define('ELFINDER_DROPBOX_META_CACHE_PATH',''); // optional for `options['metaCachePath']`
+
+/**
+ * Simple function to demonstrate how to control file access using "accessControl" callback.
+ * This method will disable accessing files/folders starting from '.' (dot)
+ *
+ * @param  string  $attr  attribute name (read|write|locked|hidden)
+ * @param  string  $path  file path relative to volume root directory started with directory separator
+ * @return bool|null
+ **/
 function access($attr, $path, $data, $volume) {
 	return strpos(basename($path), '.') === 0       // if file/folder begins with '.' (dot)
 		? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
@@ -15,7 +43,6 @@ function access($attr, $path, $data, $volume) {
 
 // Documentation for connector options:
 // https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
-
 $opts = array(
 	// 'debug' => true,
 	'roots' => array(
@@ -23,7 +50,13 @@ $opts = array(
 			'driver'        => 'LocalFileSystem',
 			'path'          => $_REQUEST["path"],
 			'URL'           => dirname($_SERVER['PHP_SELF']) . $_REQUEST["path"],
-			'accessControl' => 'access'             // disable and hide dot starting files (OPTIONAL)
+			'utf8fix'    	=> true,
+            'tmbCrop'    	=> false,
+            'tmbBgColor' 	=> 'transparent',
+            'accessControl' => 'access',
+            'quarantine' => '.tmb/.quarantine',
+            'acceptedName' => '/^.[^\.].*/'
+
 		)
 	)
 );
@@ -31,3 +64,4 @@ $opts = array(
 // run elFinder
 $connector = new elFinderConnector(new elFinder($opts));
 $connector->run();
+
